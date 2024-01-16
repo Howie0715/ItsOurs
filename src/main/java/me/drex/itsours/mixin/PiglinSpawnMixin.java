96b6b94e896a322c91791c4efbd3db01b306ac13
Howie <howie0715@gmail.com>
2024-01-16 13:06:12 +0800

    Fix mob spawning from nether portal.

---
+++ src/main/java/me/drex/itsours/mixin/PiglinSpawnMixin.java
@@ -0,0 +1,32 @@
+package me.drex.itsours.mixin;
+
+import me.drex.itsours.claim.AbstractClaim;
+import me.drex.itsours.claim.ClaimList;
+import me.drex.itsours.claim.permission.PermissionManager;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.NetherPortalBlock;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Optional;
+
+@Mixin(NetherPortalBlock.class)
+public abstract class PiglinSpawnMixin {
+
+    @Inject(
+        method = "randomTick",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+        Optional<AbstractClaim> claim = ClaimList.getClaimAt(world.toServerWorld(), pos);
+        if (claim.isPresent() && !claim.get().hasPermission(null, PermissionManager.MOB_SPAWN)) {
+            ci.cancel();
+        }
+    }
+}
